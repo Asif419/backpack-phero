a2720aa69e27a7a2248d2fda23bfd0d169e3ed38
Md. Asif Shahariar <asshah419@gmail.com>
2025-05-11 15:38:11 +0300

    problem 1 has been solved

--- src/programming.ts
+++ src/programming.ts
@@ -1,4 +1,55 @@
 {
-    const name:string = "asif";
+    // Task 1: Array filtering and mapping
+
+    interface TPerson {
+        name: string;
+        age: number;
+        gender: string;
+    }
+
+    const people: TPerson[] = [
+        {
+            name: "Rupkotha",
+            age: 28,
+            gender: "Female"
+        },
+        {
+            name: "Asif",
+            age: 34,
+            gender: "Male"
+        },
+        {
+            name: "Smith",
+            age: 22,
+            gender: "Non-binary"
+        },
+        {
+            name: "Salma",
+            age: 40,
+            gender: "Female"
+        }
+    ];
+    
+
+    function filteringFemale(people: TPerson[]): TPerson[] {
+        const peopleWithoutFemale: TPerson[] = [];
+        people.map((person: TPerson) => {
+            if (person.gender != 'Female') {
+                peopleWithoutFemale.push(person)
+            }
+        })
+        return peopleWithoutFemale;
+    }
+    const peopleWithoutFemale = filteringFemale(people);
+
+    function printNames(peopleWithoutFemale: TPerson[]): void {
+        peopleWithoutFemale.forEach(person => {
+            console.log(person.name)
+        })
+    }
+
+    // uncomment next line to check task-1
+    // printNames(peopleWithoutFemale);
+
 
 }
